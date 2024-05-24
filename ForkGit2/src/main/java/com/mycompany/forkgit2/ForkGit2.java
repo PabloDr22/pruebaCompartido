9d89c76f57b65855a7ec2b7875965ad050bc3178
PabloGarcia <pablogarciaordonez.alu@iesjulioverne.es>
2024-05-24 12:44:23 +0200

    version_4

---
+++ ForkGit2/src/main/java/com/mycompany/forkgit2/ForkGit2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.forkgit2;
+
+/**
+ *
+ * @author pablo_garcia
+ */
+public class ForkGit2 {
+
+    public static void main(String[] args) {
+        System.out.println("Pablo García");
+    }
+}
